59140d7fab8df62bb8f22b42cc950a0ec53a1b6d
Jose Sanchez <balgus@gmail.com>
2018-10-30 12:36:34 -0700

    Simple file change to test CI

--- samples/csharp_dotnetcore/51.cafe-bot/Dialogs/Shared/StateProperties/OnTurnProperty.cs
+++ samples/csharp_dotnetcore/51.cafe-bot/Dialogs/Shared/StateProperties/OnTurnProperty.cs
@@ -1,5 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+// Random change to test continuous integration
 
 using System;
 using System.Collections.Generic;
