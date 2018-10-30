6d37a1de83f0472819467f57e8c1fd4bacb0703f
Jose Sanchez <balgus@gmail.com>
2018-10-30 12:45:34 -0700

    Additional test change

--- samples/csharp_dotnetcore/13.basic-bot/BasicBot.cs
+++ samples/csharp_dotnetcore/13.basic-bot/BasicBot.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-// Random change
+// Random change again
 
 using System;
 using System.Collections.Generic;
